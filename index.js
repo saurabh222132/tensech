8aba1c817331e86bae04e1ea04d29df3d9ed09a9
saurabh222132 <skv222132@gmail.com>
2023-09-08 00:48:34 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const currencies = require("./data");
+
+try {
+  app.get("/currencies/*", (req, res) => {
+    console.log(req.originalUrl, typeof req.originalUrl);
+    //Extracting the quried currencies from the req.
+    //  Approach still to implement convert currency 1 to the USD and new multily the result with the
+    //  Currency2 and that will be your result
+    let urlString = req.originalUrl;
+
+    let currency1 = urlString.split("/")[2];
+    let currency2 = urlString.split("/")[3].split(".")[0];
+
+    console.log(currency1);
+    console.log(currency2);
+    console.log(currencies);
+    res.status(200).json({
+      date: "apply this later",
+      [currency2]: currencies.currencies[currency2],
+    });
+  });
+} catch (err) {
+  console.log(err);
+}
+
+app.listen(4000, () => {
+  console.log("server is started on port 4000");
+});
